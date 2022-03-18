70369544e3c2481326f94ede187d70c2837bd68f
dheeraj1429 <dheerajsingh1429@gmail.com>
2022-03-18 09:39:39 +0530

    setup done

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const conntion = require('./model/db');
 
 // router files
